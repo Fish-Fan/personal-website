c0c2092705f3d8c3a0290e7012c513c1198d4a13
Fish-Fan <fanyank@126.com>
2016-03-16 17:22:36 +0800

    first C program

---
+++ Documents/code/HF.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{int fahr = 0;
+
+	float celsius = 0;
+
+	for(fahr = 0;fahr <= 300;fahr = fahr + 20)
+
+		printf("%3d\t%6.1f\n",fahr,celsius = (5.0/9.0)*(fahr - 32) );
+	return 0;
+}
